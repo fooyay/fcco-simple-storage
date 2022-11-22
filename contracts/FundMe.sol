690483f065448caeed94bf448e075f276233b79b
fooyay <sean.coates@gmail.com>
2022-11-22 15:22:56 -0500

    fundme exercise

---
+++ contracts/FundMe.sol
@@ -0,0 +1,74 @@
+// Get funds from users
+// Withdraw funds
+// Set a minimum funding value in USD
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "./PriceConverter.sol";
+
+error NotOwner();
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public constant MINIMUM_USD = 50 * 1e18; // relative to Wei
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    address public immutable i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable {
+        // Want to be able to set a minimum fund amount in USD
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough!");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        // reset the array
+        funders = new address[](0);
+        // actually withraw the funds
+
+        // msg.sender = address, payable(msg.sneder) = payable address
+
+        // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+        // call
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+
+    }
+
+    modifier onlyOwner {
+        // require(msg.sender == i_owner, "Sender is not owner!");
+        if(msg.sender != i_owner) { revert NotOwner(); }
+        _;
+    }
+
+    // What happens if someone sends this contract ETH without calling the fund function?
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+}
